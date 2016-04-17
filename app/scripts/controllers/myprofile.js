baaf2f35078f49e7a0b7db873208b2dc2815592d
tuxming <tuxming@gmail.com>
2016-04-17 23:29:34 +0800

    myprofile.html page

--- app/scripts/controllers/myprofile.js
+++ app/scripts/controllers/myprofile.js
@@ -21,5 +21,10 @@ var app = angular.module('app', [
 
 app.controller('myprofileController', function($scope, g){
 	//console.log(window.g.server);
-	console.log(g.server);
+	//console.log(g.server);
+	$scope.array = [0,1,2,3,4,5,6,7,8,9];
+	
+	$scope.array1 = [0,1,2];
+	
+	
 });
