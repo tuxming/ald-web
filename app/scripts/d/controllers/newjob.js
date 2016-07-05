50f6e3e880e6e736c1cc3cc8c0a72e15920ce309
caochao <caochao@prcsteel.com>
2016-07-05 21:26:05 +0800

    添加newjob.js

---
+++ app/scripts/d/controllers/newjob.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+
+/**
+ * @ngdoc function
+ * @name app.controller:newjobCtrl
+ * @description
+ * # newjobCtrl
+ * Controller of the app
+ */
+app.controller('newjobCtrl', function ($scope) {
+    $scope.previousjob = '';
+    $scope.message = '';
+
+});
